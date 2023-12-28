553512b870ce4048ac61768a2c2ce062c8660463
DavidLapadula <lapadula.david@gmail.com>
2023-12-27 22:19:16 -0500

    Convert json to csv functionality done

--- src/app/utils/converters.js
+++ src/app/utils/converters.js
@@ -6,18 +6,16 @@ const jsonToCsv = (data) => {
   var json = parseJson(data);
   if (!json) return null;
 
-  if (Array.isArray(json)) {
-    return json.map(item => {
-      return buildCSVFromJson(item);
-    }).join('');
-  } else {
-    return buildCSVFromJson(json);
-  }
+  let isArray = Array.isArray(json);
+  let headers = buildHeadersArray(isArray, json);
+  let rowData = buildRowData(isArray, headers, json);
+
+  return headers.map(item => `"${item}"`).join(',') + "\r\n" + rowData;
 }
 
-const parseJson = (str) => {
+const parseJson = (data) => {
   try {
-    var json = JSON.parse(str);
+    var json = JSON.parse(data);
     if (typeof json === 'object') return json;
     return false;
   } catch (e) {
@@ -25,14 +23,41 @@ const parseJson = (str) => {
   }
 }
 
-const isValidCSV = (data) => {
+const parseCsv = (data) => {
 
 }
 
-const buildCSVFromJson = (json) => {
-  let keys = Object.keys(json).join(',') + ",";
-  let values = Object.values(json).join(',') + ",";
-  return keys + "\r\n" + values + "\r\n" 
+
+const buildHeadersArray = (isArray, json) => {
+  if (!isArray) return Object.keys(json); 
+
+  let headerSet = new Set(); 
+
+  for (let item of json) {
+    Object.keys(item).forEach(header => {
+      headerSet.add(header);
+    });
+  }
+
+  return Array.from(headerSet);
+}
+
+const buildRowData = (isArray, headers, json) => {
+  let rowData; 
+
+  if (isArray) {
+    rowData = json.map(obj => {
+        return headers.map(header => {
+          return obj.hasOwnProperty(header) ? `"${obj[header]}"` : `""`
+        }).join(',') + "\r\n"; 
+    }).join(''); 
+  } else {
+    rowData = headers.map(header => {
+      return json.hasOwnProperty(header) ? `"${json[header]}"` : `""`
+    }).join(',') + "\r\n"; 
+  }
+
+  return rowData;
 }
 
 export { csvToJson, jsonToCsv }; 
