babed12c49e82af96b787353e5fd0fe66de7750e
DavidLapadula <lapadula.david@gmail.com>
2024-01-06 16:17:00 -0500

    Clean up unit tests

--- __tests__/converters.test.js
+++ __tests__/converters.test.js
@@ -2,66 +2,73 @@ const { csvToJson, jsonToCsv } = require('../src/app/utils/converters');
 // const { Home } = require('../src/app/page');
 // const { Dropdown } = require('../src/app/components/Dropdown');
 
-const invalidJson = "123";
-const invalidJsonObject = `{"name": "John Doe","age": 30"city": "New York"}`; 
-const validJsonObject = `{"name": "Jane Doe","age": 25,"city": "Chicago"}`; 
-const tired = `
+const invalidJson = `{"name": "John Doe","age": 30"city": "New York"}`; 
+const validJson = `{"name": "Jane Doe","age": 25,"city": "Chicago"}`; 
+const validJsonAsCsv = `
 "name","age","city"
 "Jane Doe","25","Chicago"
 `
-const invalidCsvString = `Name,Age,City`
-const validCsvStringOneRow = `Name,Age,City
+const invalidCsv = `Name,Age,City`
+const validSingleRowCSV = `Name,Age,City
 John,30,New York`;
-const validCsvStringTwoRows = `Name,Age,City
+const validMultiRowCsv = `Name,Age,City
 John,30,New York
 Jane,25,Denver`;
-const validCsvAsJsonOneRow = "{\"Name\":\"John\",\"Age\":\"30\",\"City\":\"New York\"}";
-const validCsvAsJsonTwoRows = "[{\"Name\":\"John\",\"Age\":\"30\",\"City\":\"New York\"},{\"Name\":\"Jane\",\"Age\":\"25\",\"City\":\"Denver\"}]";
-
-console.log(jsonToCsv(validJsonObject))
+const validOneRowCsvAsJson = "{\"Name\":\"John\",\"Age\":\"30\",\"City\":\"New York\"}";
+const validMultiRowCsvAsJson = "[{\"Name\":\"John\",\"Age\":\"30\",\"City\":\"New York\"},{\"Name\":\"Jane\",\"Age\":\"25\",\"City\":\"Denver\"}]";
 
 
+/*
+    Main application page render test
+*/
 // test('renders Dropdown', () => {
 //     render(<Home />);
 //     expect(Dropdown).toBeInTheDocument();
 //   });
 
-test('Invalid json string to csv', () => {
+/*
+    JSON to CSV tests
+*/
+
+test('JSON to CSV: null', () => {
+    expect(jsonToCsv(null)).toBe(null);
+});
+
+test('JSON to CSV: Invalid string', () => {
+    expect(jsonToCsv('123')).toBe(null);
+});
+
+test('JSON to CSV: Invalid object', () => {
     expect(jsonToCsv(invalidJson)).toBe(null);
 });
 
-test('invalid json object to csv', () => {
-    expect(jsonToCsv(invalidJsonObject)).toBe(null);
+// test('JSON to CSV: Valid object', () => {
+//     expect(jsonToCsv(validJson)).toBe(validJsonAsCsv)
+// });
+
+/*
+    CSV to JSON tests
+*/
+test('CSV to JSON: Invalid string', () => {
+    expect(csvToJson(invalidCsv)).toBe(null);
 });
 
-test('"" json object to csv', () => {
-    expect(jsonToCsv("")).toBe(null);
-});
-
-test('valid json object to csv', () => {
-    expect(jsonToCsv(validJsonObject)).toBe(tired)
-});
-
-test('invalid csv to json', () => {
-    expect(csvToJson(invalidCsvString)).toBe(null);
-});
-
-test('null csv to json', () => {
+test('CSV to JSON: null', () => {
     expect(csvToJson(null)).toBe(null);
 });
 
-test('[] csv to json', () => {
+test('CSV to JSON: Empty array', () => {
     expect(csvToJson([])).toBe(null);
 });
 
-test('"" csv to json', () => {
+test('CSV to JSON: Blank space', () => {
     expect(csvToJson(" ")).toBe(null);
 });
 
-test('valid csv with two rows to json', () => {
-    expect(csvToJson(validCsvStringTwoRows)).toEqual(validCsvAsJsonTwoRows);
+test('CSV to JSON: Valid single row CSV', () => {
+    expect(csvToJson(validMultiRowCsv)).toEqual(validMultiRowCsvAsJson);
 });
 
-test('valid csv with one row to json', () => {
-    expect(csvToJson(validCsvStringOneRow)).toEqual(validCsvAsJsonOneRow);
+test('CSV to JSON: Valid multi row CSV', () => {
+    expect(csvToJson(validSingleRowCSV)).toEqual(validOneRowCsvAsJson);
 });
